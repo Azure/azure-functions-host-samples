d8f8240c890437a123ad007d5e7db8ce9f68ee8b
Mathew Charles <mathewc@microsoft.com>
2017-02-16 18:11:12 -0800

    Fixing missing package reference.

--- WebJobScript/Program.cs
+++ WebJobScript/Program.cs
@@ -14,7 +14,7 @@ namespace WebJobScript
             // When deployed as a WebJob, the WebJobs infrastructure handles
             // watching for script changes and restarting the host, so we
             // need to disable ScriptHost's inbuilt file watching.
-            ScriptHostConfiguration config = new ScriptHostConfiguration()
+            ScriptHostConfiguration config = new ScriptHostConfiguration
             {
                 RootScriptPath = functionsPath,
                 FileWatchingEnabled = false
